46d3227c88e6a9e7ced89c3510db5ca536dc64b6
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-09-22 00:09:06 +0200

    test: add negative test for risk_level

--- test/api/alert_test.exs
+++ test/api/alert_test.exs
@@ -38,6 +38,17 @@ defmodule CaseManager.Alerts.AlertTest do
     assert {:error, _changeset} = Ash.create(changeset)
   end
 
+  test "fails to create an alert with an invalid risk_level", %{team: team} do
+    changeset =
+      Alert
+      |> Ash.Changeset.for_create(
+        :create,
+        Map.put(@valid_alert_attrs, :team_id, team.id)
+        |> Map.put(:risk_level, "Invalid")
+      )
+    assert {:error, _changeset} = Ash.create(changeset)
+  end
+
   test "fails to create an alert with an invalid team_id" do
     changeset =
       Alert
