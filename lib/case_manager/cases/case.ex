771a1b7edec3ea041642aa71995462a076e79a54
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-09-30 10:19:55 +0200

    api: migrate initial case resource

---
+++ lib/case_manager/cases/case.ex
@@ -0,0 +1,107 @@
+defmodule CaseManager.Cases.Case do
+  @moduledoc """
+  Resource that represents a single case in the system.
+  """
+  use Ash.Resource,
+    otp_app: :case_manager,
+    domain: CaseManager.Cases,
+    data_layer: AshPostgres.DataLayer,
+    notifiers: [Ash.Notifier.PubSub]
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :title, :string do
+      allow_nil? false
+    end
+
+    attribute :description, :string
+
+    attribute :status, :string do
+      allow_nil? false
+      default "In Progress"
+    end
+
+    attribute :priority, :string do
+      allow_nil? false
+    end
+
+    attribute :team_id, :uuid do
+      allow_nil? false
+    end
+
+    attribute :escalated, :boolean do
+      allow_nil? false
+    end
+
+    timestamps()
+  end
+
+  resource do
+    plural_name :cases
+  end
+
+  pub_sub do
+    module CaseManagerWeb.Endpoint
+
+    prefix "case"
+    publish :create, ["created"]
+  end
+
+  postgres do
+    table "cases"
+    repo CaseManager.Repo
+
+    references do
+      reference :team, on_delete: :delete, on_update: :update, name: "case_to_team_fkey"
+    end
+  end
+
+  validations do
+    validate one_of(:status, ["In Progress", "Pending", "Closed", "Benign"])
+    validate one_of(:priority, ["Informational", "Low", "Medium", "High", "Critical"])
+  end
+
+  actions do
+    create :create do
+      accept [
+        :title,
+        :description,
+        :status,
+        :priority,
+        :escalated,
+        :team_id
+      ]
+
+      change fn changeset, _context ->
+        changeset
+        |> Ash.Changeset.get_attribute(:priority)
+        |> case do
+          nil ->
+            changeset
+
+          priority ->
+            Ash.Changeset.change_attribute(changeset, :priority, String.capitalize(priority))
+        end
+      end
+    end
+
+    read :read do
+      primary? true
+      prepare build load: [:team]
+
+      pagination do
+        required? true
+        offset? true
+        countable true
+        default_limit 20
+      end
+    end
+  end
+
+  relationships do
+    belongs_to :team, CaseManager.Teams.Team do
+      allow_nil? false
+    end
+  end
+end
