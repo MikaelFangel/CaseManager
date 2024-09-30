ee2db05db9b11a7d5a24aa8772cf6713149fd448
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-09-30 22:59:44 +0200

    api: create contact info resources

---
+++ lib/case_manager/contact_infos/phone.ex
@@ -0,0 +1,28 @@
+defmodule CaseManager.ContactInfos.Phone do
+  use Ash.Resource,
+    otp_app: :case_manager,
+    domain: CaseManager.ContactInfos,
+    data_layer: AshPostgres.DataLayer
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :country_code, :string
+
+    attribute :phone, :string do
+      allow_nil? false
+    end
+
+    timestamps()
+  end
+
+  postgres do
+    table "phone"
+    repo CaseManager.Repo
+  end
+
+  actions do
+    defaults [:read, :destroy, create: :*, update: :*]
+  end
+  
+end
