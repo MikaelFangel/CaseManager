096942adede93a8c9c6852f7e3a671c2a84d421a
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-10-02 13:39:19 +0200

    test: test relation for team and email

--- test/api/team_internal_test.exs
+++ test/api/team_internal_test.exs
@@ -1,7 +1,9 @@
 defmodule CaseManager.TeamInternalTest do
   use CaseManager.DataCase, async: true
   use ExUnitProperties
+  alias CaseManager.ContactInfos.Email
   alias CaseManager.Teams.Team
+  alias CaseManager.Relationships.TeamEmail
   alias CaseManagerWeb.TeamGenerator
 
   describe "postive tests for creating teams" do
@@ -34,5 +36,37 @@ defmodule CaseManager.TeamInternalTest do
         assert {:error, _team} = Ash.create(changeset)
       end
     end
+  end
+
+  describe "postive relationship test for teams" do
+    property "teams can have 0 or more email relationships" do
+      check all(
+              team_attr <- TeamGenerator.team_attrs(),
+              emails <-
+                StreamData.list_of(StreamData.string(:printable, min_length: 1), min_length: 1)
+            ) do
+        team = Team |> Ash.Changeset.for_create(:create, team_attr) |> Ash.create!()
+
+        email_ids =
+          emails
+          |> Enum.map(fn gen_mail ->
+            email =
+              Email |> Ash.Changeset.for_create(:create, %{email: gen_mail}) |> Ash.create!()
+
+            TeamEmail
+            |> Ash.Changeset.for_create(:create, %{team_id: team.id, email_id: email.id})
+            |> Ash.create!()
+
+            email.id
+          end)
+
+        team_with_email = Ash.load!(team, :email)
+        assert length(team_with_email.email) === length(emails)
+        assert Enum.sort(email_ids) === team_with_email.email |> Enum.map(& &1.id) |> Enum.sort()
+      end
+    end
+  end
+
+  describe "negative relationship test for teams" do
   end
 end
