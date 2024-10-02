03032b0effa95322c5880f18fc1f6b1759696ae8
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-10-02 11:15:40 +0200

    test: add team generator to existing tests and group them

--- test/api/alert_external_test.exs
+++ test/api/alert_external_test.exs
@@ -7,49 +7,49 @@ defmodule CaseManager.AlertExternalTest do
   use Plug.Test
   alias CaseManager.Teams.Team
   alias CaseManagerWeb.AlertGenerator
-
-  @valid_team_attrs %{
-    name: "Test Team"
-  }
+  alias CaseManagerWeb.TeamGenerator
 
   setup do
-    {:ok, team} = Team |> Ash.Changeset.for_create(:create, @valid_team_attrs) |> Ash.create()
+    [gen_team] = TeamGenerator.team_attrs() |> Enum.take(1)
+    {:ok, team} = Team |> Ash.Changeset.for_create(:create, gen_team) |> Ash.create()
     {:ok, team: team}
   end
 
-  property "valid alert attributes return a 201 created response", %{team: team} do
-    check all(alert_attr <- AlertGenerator.alert_attrs()) do
-      data = %{data: %{type: "alert", attributes: alert_attr |> Map.put(:team_id, team.id)}}
-      json_data = Jason.encode!(data)
+  describe "JSON API POST" do
+    property "valid alert attributes return a 201 created response", %{team: team} do
+      check all(alert_attr <- AlertGenerator.alert_attrs()) do
+        data = %{data: %{type: "alert", attributes: alert_attr |> Map.put(:team_id, team.id)}}
+        json_data = Jason.encode!(data)
 
-      conn =
-        conn(:post, "/api/json/alerts", json_data)
-        |> put_req_header("accept", "application/vnd.api+json")
-        |> put_req_header("content-type", "application/vnd.api+json")
-        |> CaseManagerWeb.Router.call(CaseManagerWeb.Endpoint)
+        conn =
+          conn(:post, "/api/json/alerts", json_data)
+          |> put_req_header("accept", "application/vnd.api+json")
+          |> put_req_header("content-type", "application/vnd.api+json")
+          |> CaseManagerWeb.Router.call(CaseManagerWeb.Endpoint)
 
-      assert conn.status == 201
+        assert conn.status == 201
+      end
     end
-  end
 
-  property "invalid alert attributes return a 400 bad request response", %{team: team} do
-    check all(alert_attr <- AlertGenerator.alert_attrs()) do
-      data = %{
-        data: %{
-          type: "alert",
-          attributes: alert_attr |> Map.put(:team_id, team.id) |> Map.put(:title, nil)
+    property "invalid alert attributes return a 400 bad request response", %{team: team} do
+      check all(alert_attr <- AlertGenerator.alert_attrs()) do
+        data = %{
+          data: %{
+            type: "alert",
+            attributes: alert_attr |> Map.put(:team_id, team.id) |> Map.put(:title, nil)
+          }
         }
-      }
 
-      json_data = Jason.encode!(data)
+        json_data = Jason.encode!(data)
 
-      conn =
-        conn(:post, "/api/json/alerts", json_data)
-        |> put_req_header("accept", "application/vnd.api+json")
-        |> put_req_header("content-type", "application/vnd.api+json")
-        |> CaseManagerWeb.Router.call(CaseManagerWeb.Endpoint)
+        conn =
+          conn(:post, "/api/json/alerts", json_data)
+          |> put_req_header("accept", "application/vnd.api+json")
+          |> put_req_header("content-type", "application/vnd.api+json")
+          |> CaseManagerWeb.Router.call(CaseManagerWeb.Endpoint)
 
-      assert conn.status == 400
+        assert conn.status == 400
+      end
     end
   end
 end
