312f4547805c22d8b66ae504920b9bea4d38fe96
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-10-03 22:38:07 +0200

    policy: add mssp creation policy

---
+++ lib/case_manager/checks/mssp_create_policy.ex
@@ -0,0 +1,18 @@
+defmodule CaseManager.Policies.MSSPCreatePolicy do
+  use Ash.Policy.SimpleCheck
+
+  def describe(_options) do
+    "check that the actor is part of a MSSP team"
+  end
+
+  def match?(
+        %CaseManager.Teams.User{} = actor,
+        _context,
+        _opts
+      ) do
+    team = Ash.load!(actor, :team).team
+    team && team.type == "MSSP"
+  end
+
+  def match?(_actor, _changeset, _opts), do: false
+end
