56a92a1672fa4880408da01ffe910b2860bbd378
Silja Sandersen <91070829+yech199@users.noreply.github.com>
2024-10-07 16:02:27 +0200

    move core JS commands to its own file

--- lib/case_manager_web/components/core_components.ex
+++ lib/case_manager_web/components/core_components.ex
@@ -16,6 +16,7 @@ defmodule CaseManagerWeb.CoreComponents do
   """
   use Phoenix.Component
   import CaseManagerWeb.Icon
+  import CaseManagerWeb.CoreJsCmds
 
   alias Phoenix.LiveView.JS
   use Gettext, backend: CaseManagerWeb.Gettext
@@ -185,26 +186,6 @@ defmodule CaseManagerWeb.CoreComponents do
 
   ## JS Commands ---------------------------------------------------------
 
-  def show(js \\ %JS{}, selector) do
-    JS.show(js,
-      to: selector,
-      transition:
-        {"transition-all transform ease-out duration-300",
-         "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
-         "opacity-100 translate-y-0 sm:scale-100"}
-    )
-  end
-
-  def hide(js \\ %JS{}, selector) do
-    JS.hide(js,
-      to: selector,
-      time: 200,
-      transition:
-        {"transition-all transform ease-in duration-200",
-         "opacity-100 translate-y-0 sm:scale-100",
-         "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"}
-    )
-  end
 
   def show_modal(js \\ %JS{}, id) when is_binary(id) do
     js
