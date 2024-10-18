f723da61392413ee1cd977dacb32174e9b0ffee5
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-10-18 21:59:55 +0200

    ui: redirect if no alerts selected

--- lib/case_manager_web/live/case_live/new.ex
+++ lib/case_manager_web/live/case_live/new.ex
@@ -4,12 +4,18 @@ defmodule CaseManagerWeb.CaseLive.New do
   def mount(_params, _session, socket) do
     selected_alerts =
       CaseManager.SelectedAlerts.get_selected_alerts(socket.assigns.current_user.id)
+      |> Enum.map(fn alert_id -> {alert_id, Ash.get!(CaseManager.Alerts.Alert, alert_id)} end)
 
-    selected_alerts =
-      Enum.map(selected_alerts, fn a -> {a, Ash.get!(CaseManager.Alerts.Alert, a)} end)
+    # Redirect users if they try to access the page through the URL without selecting any alerts
+    # else assign the selected alerts to the socket and render the page.
+    case selected_alerts do
+      [] ->
+        {:ok, push_navigate(socket, to: ~p"/alerts")}
 
-    {:ok,
-     assign(socket, :selected_alerts, selected_alerts)
-     |> assign(current_user: socket.assigns.current_user)}
+      _alerts ->
+        {:ok,
+         assign(socket, :selected_alerts, selected_alerts)
+         |> assign(current_user: socket.assigns.current_user)}
+    end
   end
 end
