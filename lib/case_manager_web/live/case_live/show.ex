13708ab01aae31e5db0d49742814924f4620fe73
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-10-30 12:29:28 +0100

    feat: support newline characters and long messages

--- lib/case_manager_web/live/case_live/show.ex
+++ lib/case_manager_web/live/case_live/show.ex
@@ -54,4 +54,11 @@ defmodule CaseManagerWeb.CaseLive.Show do
 
     {:noreply, socket |> assign(case: updated_case)}
   end
+
+  def newline_to_br(text) do
+    text
+    |> String.split("\n")
+    |> Enum.map_join("<br/>", & &1)
+    |> raw()
+  end
 end
