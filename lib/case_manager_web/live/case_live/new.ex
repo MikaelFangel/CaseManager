c5d17ef3b049c467ef3ad0250ce1cee41fd8bde6
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-11-03 20:05:48 +0100

    chore: use anonymous function for finding the team name

--- lib/case_manager_web/live/case_live/new.ex
+++ lib/case_manager_web/live/case_live/new.ex
@@ -7,29 +7,32 @@ defmodule CaseManagerWeb.CaseLive.New do
       CaseManager.SelectedAlerts.get_selected_alerts(socket.assigns.current_user.id)
       |> Enum.map(fn alert_id -> {alert_id, Ash.get!(CaseManager.Alerts.Alert, alert_id)} end)
 
-    team_name =
-      selected_alerts
-      |> Enum.take(1)
-      |> then(fn [{_id, alert} | _xs] -> alert.team.name end)
-
-    form =
-      CaseManager.Cases.Case
-      |> Form.for_create(:create, forms: [auto?: true])
-      |> to_form()
-
-    socket =
-      socket
-      |> assign(:menu_item, nil)
-      |> assign(:current_user, socket.assigns.current_user)
-      |> assign(:team_name, team_name)
-      |> assign(:related_alerts, selected_alerts)
-      |> assign(:form, form)
-
     # Redirect users if they try to access the page through the URL without selecting any alerts
     # else assign the selected alerts to the socket and render the page.
     case selected_alerts do
-      [] -> {:ok, push_navigate(socket, to: ~p"/alerts")}
-      _alerts -> {:ok, socket, layout: {CaseManagerWeb.Layouts, :app_m0}}
+      [] ->
+        {:ok, push_navigate(socket, to: ~p"/alerts")}
+
+      _alerts ->
+        form =
+          CaseManager.Cases.Case
+          |> Form.for_create(:create, forms: [auto?: true])
+          |> to_form()
+
+        first_team_name = fn
+          [{_id, alert} | _rest] -> alert.team.name
+          _empty -> nil
+        end
+
+        socket =
+          socket
+          |> assign(:menu_item, nil)
+          |> assign(:current_user, socket.assigns.current_user)
+          |> assign(:team_name, first_team_name.(selected_alerts))
+          |> assign(:related_alerts, selected_alerts)
+          |> assign(:form, form)
+
+        {:ok, socket, layout: {CaseManagerWeb.Layouts, :app_m0}}
     end
   end
 end
