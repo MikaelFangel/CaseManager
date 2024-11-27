863e5de59c618a262921c5830d58131b5a4e701b
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-11-27 12:27:02 +0100

    feat: add dynamic logo and background to screens

--- lib/case_manager_web/helpers.ex
+++ lib/case_manager_web/helpers.ex
@@ -1,15 +1,31 @@
 defmodule CaseManagerWeb.Helpers do
   @moduledoc """
-  Module that defines helper function for the web based part of the application.
+  Module that defines helper function for the web-based part of the application.
   """
 
+  alias CaseManager.AppConfig.Setting
+
   @doc """
-  Render basic html from text with markdown. All html tags is stripped before returning it as a raw value.
+  Render basic HTML from text with markdown. All HTML tags are stripped before returning it as a raw value.
   """
   def render_markdown!(text) do
     text
     |> HtmlSanitizeEx.strip_tags()
     |> Earmark.as_html!()
     |> Phoenix.HTML.raw()
+  end
+
+  def load_bg do
+    case Ash.get(Setting, %{key: "background"}) do
+      {:ok, setting} -> hd(Ash.load!(setting, [:file]).file)
+      {:error, _error} -> nil
+    end
+  end
+
+  def load_logo do
+    case Ash.get(Setting, %{key: "logo"}) do
+      {:ok, setting} -> hd(Ash.load!(setting, [:file]).file)
+      {:error, _error} -> nil
+    end
   end
 end
