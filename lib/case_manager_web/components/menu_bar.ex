07103888ef8b0590765444a81c3310d8168863f3
Silja Sandersen <91070829+yech199@users.noreply.github.com>
2024-11-29 13:54:15 +0100

    feat: add restrictions to menu_bar
    - Only admin users can manage users
    - Only admin users in MSSP teams can manage teams

--- lib/case_manager_web/components/menu_bar.ex
+++ lib/case_manager_web/components/menu_bar.ex
@@ -31,11 +31,14 @@ defmodule CaseManagerWeb.MenuBar do
 
           <.menu_item icon_name="hero-document-duplicate" active?={@current_page == :cases} path="/" />
 
-          <%= if @current_user.team_type==:mssp do %>
+          <%= if @current_user.role==:admin do %>
+            <% IO.inspect(@current_user.role, label: "Role") %>
             <div class="w-full border border-neutral-500"></div>
             <.menu_item icon_name="hero-users" active?={@current_page == :users} path="/users" />
-            <div class="w-full border border-neutral-500"></div>
-            <.menu_item icon_name="hero-building-office" active?={@current_page == :teams} path="/teams" />
+            <%= if @current_user.team_type==:mssp do %>
+              <div class="w-full border border-neutral-500"></div>
+              <.menu_item icon_name="hero-building-office" active?={@current_page == :teams} path="/teams" />
+            <% end %>
           <% end %>
         </div>
         <!-- Bottom content -->
