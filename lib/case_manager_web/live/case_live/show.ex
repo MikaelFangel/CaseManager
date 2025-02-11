65640db711ed8257e947f0a47ba1c7de104428c5
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-02-11 09:35:07 +0100

    fix: load the user on comments sent using Phoenix broadcast

--- lib/case_manager_web/live/case_live/show.ex
+++ lib/case_manager_web/live/case_live/show.ex
@@ -23,7 +23,7 @@ defmodule CaseManagerWeb.CaseLive.Show do
         %Phoenix.Socket.Broadcast{event: "create", payload: %Ash.Notifier.Notification{data: comment}},
         socket
       ) do
-    comment = Map.put(comment, :header, nil)
+    comment = comment |> Ash.load!(:user) |> Map.put(:header, nil)
     {:noreply, stream_insert(socket, :comments, comment, at: 0)}
   end
 
