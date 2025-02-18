09d62df3b1e30ea988481617912907d618d8182f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-02-18 15:56:31 +0100

    test: add test for routes redirections

--- test/live/login_test.exs
+++ test/live/login_test.exs
@@ -1,13 +1,23 @@
 defmodule CaseManager.Live.LoginTest do
   use CaseManagerWeb.ConnCase, async: true
 
-  describe "/" do
-    test "loads login screen when not logged in", %{conn: conn} do
-      conn
-      |> visit("/")
-      |> assert_has("button", text: "Sign in")
-    end
+  @public_routes [
+    "/sign-in",
+    "/onboarding",
+    "/onboarding/team",
+    "/onboarding/user"
+  ]
 
+  @protected_routes [
+    "/users",
+    "/alerts",
+    "/case/new",
+    "/teams",
+    "/settings",
+    "/user"
+  ]
+
+  describe "public routes" do
     test "user can sign in", %{conn: conn} do
       user = generate(user())
 
@@ -17,6 +27,26 @@ defmodule CaseManager.Live.LoginTest do
       |> fill_in("Password", with: "password")
       |> submit()
       |> refute_has("button", text: "Sign in")
+    end
+
+    for route <- @public_routes do
+      @route route
+      test "GET #{@route} public pages doesn't redirect", %{conn: conn} do
+        conn
+        |> visit(@route)
+        |> assert_path(@route)
+      end
+    end
+  end
+
+  describe "protected routes" do
+    for route <- @protected_routes do
+      @route route
+      test "GET #{@route} redirects to login when not authenticated", %{conn: conn} do
+        conn
+        |> visit(@route)
+        |> assert_has("button", text: "Sign in")
+      end
     end
   end
 end
