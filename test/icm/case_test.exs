84ad2c22165bf42c2e91ff2aa5f1183197631511
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-02-19 14:47:01 +0100

    test: add initial case test

--- test/icm/case_test.exs
+++ test/icm/case_test.exs
@@ -9,4 +9,25 @@ defmodule CaseManager.ICM.CaseTest do
       assert CaseManager.ICM.list_cases!(actor: user).results == []
     end
   end
+
+  describe "CaseManager.ICM.get_case_by_id" do
+    # Skipped because the user.team_type is not loaded and thus makes test fail
+    @tag :skip
+    test "a case can only be read by an mssp user or one related to the same team" do
+      mssp_team = generate(team(type: :mssp))
+      mssp_user = generate(user(team_id: mssp_team.id))
+
+      customer_team = generate(team(type: :customer))
+      customer_user = generate(user(team_id: customer_team.id))
+
+      other_team = generate(team(type: :customer))
+      other_user = generate(user(team_id: other_team.id))
+
+      case = generate(case(team_id: customer_team.id))
+
+      assert CaseManager.ICM.can_get_case_by_id?(mssp_user, case.id, data: case)
+      assert CaseManager.ICM.can_get_case_by_id?(customer_user, case.id, data: case)
+      refute CaseManager.ICM.can_get_case_by_id?(other_user, case.id, data: case)
+    end
+  end
 end
