02d6c89f31784cdc80926037cf9f954548a67709
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-02-24 11:13:24 +0100

    chore: set user from bearer

--- lib/case_manager_web/ash_json_api_router.ex
+++ lib/case_manager_web/ash_json_api_router.ex
@@ -5,5 +5,6 @@ defmodule CaseManagerWeb.AshJsonApiRouter do
   use AshJsonApi.Router,
     domains: [Module.safe_concat(["CaseManager.ICM"]), Module.safe_concat(["CaseManager.Teams"])],
     open_api: "/open_api",
-    open_api_title: "CaseManager API Documentation"
+    open_api_title: "CaseManager API Documentation",
+    open_api_version: to_string(Application.spec(:tunez, :vsn))
 end
