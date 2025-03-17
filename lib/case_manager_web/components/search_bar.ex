4e31454a5e74e4e18baad36ccac07a8939b5a352
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-03-17 14:10:10 +0100

    feat: make the search run as you type

--- lib/case_manager_web/components/search_bar.ex
+++ lib/case_manager_web/components/search_bar.ex
@@ -11,11 +11,20 @@ defmodule CaseManagerWeb.SearchBar do
   attr :value, :string, default: ""
   attr :class, :string, default: "hidden sm:inline"
   attr :input_class, :string, default: "!inline-block !w-fit"
+  attr :rest, :global
 
   def search_bar(assigns) do
     ~H"""
     <form data-role="search" class={@class} phx-submit={@action}>
-      <.input type="search" id="search" name="search" placeholder={@placeholder} value={@value} class={@input_class} />
+      <.input
+        type="search"
+        id="search"
+        name="search"
+        placeholder={@placeholder}
+        value={@value}
+        class={@input_class}
+        {@rest}
+      />
     </form>
     """
   end
