c8e59a3aa897fc41ca4b67f45b6cf98abb9f3f06
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-03-18 14:49:08 +0100

    test: enable test for who can read an alert

--- test/icm/alert_test.exs
+++ test/icm/alert_test.exs
@@ -12,19 +12,20 @@ defmodule CaseManager.ICM.AlertTest do
   end
 
   describe "CaseManager.ICM.get_alert_by_id" do
-    # Skipped because policies is not implemented for alerts yet.
-    @tag :skip
-    test "only mssp users can get alerts from a team not belonging to them" do
+    test "only admin and soc users can get alerts from a team not belonging to them" do
       alert = generate(alert())
 
-      mssp_team = generate(team(type: :mssp))
-      mssp_user = generate(user(team_id: mssp_team.id))
+      admin = generate(user(role: :admin))
+      soc_admin = generate(user(role: :soc_admin))
+      soc_analyst = generate(user(role: :soc_analyst))
+      team_admin = generate(user(role: :team_admin))
+      team_member = generate(user(role: :team_member))
 
-      customer_team = generate(team(type: :customer))
-      customer_user = generate(user(team_id: customer_team.id))
-
-      assert CaseManager.ICM.can_get_alert_by_id?(mssp_user, alert.id, data: alert)
-      refute CaseManager.ICM.can_get_alert_by_id?(customer_user, alert.id, data: alert)
+      assert CaseManager.ICM.can_get_alert_by_id?(admin, alert.id, data: alert)
+      assert CaseManager.ICM.can_get_alert_by_id?(soc_admin, alert.id, data: alert)
+      assert CaseManager.ICM.can_get_alert_by_id?(soc_analyst, alert.id, data: alert)
+      refute CaseManager.ICM.can_get_alert_by_id?(team_admin, alert.id, data: alert)
+      refute CaseManager.ICM.can_get_alert_by_id?(team_member, alert.id, data: alert)
     end
   end
 
