e3a8a4bce704148c95431e97ead89e89bf744225
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-03-18 09:02:27 +0100

    chore: refactor module

--- lib/case_manager_web/live/case_live/index.ex
+++ lib/case_manager_web/live/case_live/index.ex
@@ -55,27 +55,13 @@ defmodule CaseManagerWeb.CaseLive.Index do
 
   @impl true
   def handle_event("search", %{"search" => search}, socket) do
-    params =
-      remove_empty(%{
-        q: search,
-        filter: socket.assigns[:filter],
-        sort_by: socket.assigns[:sort_by],
-        team_name: socket.assigns[:team_filter]
-      })
-
+    params = update_params(socket, %{q: search})
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
   @impl true
   def handle_event("change-team", %{"team_filter" => team_name}, socket) do
-    params =
-      remove_empty(%{
-        q: socket.assigns[:search],
-        filter: socket.assigns[:filter],
-        sort_by: socket.assigns[:sort_by],
-        team_name: team_name
-      })
-
+    params = update_params(socket, %{team_name: team_name})
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
@@ -88,27 +74,13 @@ defmodule CaseManagerWeb.CaseLive.Index do
         _invalid -> %{}
       end
 
-    params =
-      remove_empty(%{
-        q: socket.assigns[:search],
-        filter: filter,
-        sort_by: socket.assigns[:sort_by],
-        team_name: socket.assigns[:team_filter]
-      })
-
+    params = update_params(socket, %{filter: filter})
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
   @impl true
   def handle_event("change-sort", %{"sort_by" => sort}, socket) do
-    params =
-      remove_empty(%{
-        q: socket.assigns[:search],
-        filter: socket.assigns[:filter],
-        sort_by: sort,
-        team_name: socket.assigns[:team_filter]
-      })
-
+    params = update_params(socket, %{sort_by: sort})
     {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
@@ -148,47 +120,28 @@ defmodule CaseManagerWeb.CaseLive.Index do
 
   def filter_changer(assigns) do
     assigns = assign(assigns, :options, filter_options())
-
-    ~H"""
-    <form data-role="cases-filter" class="hidden sm:inline" phx-change="change-filter">
-      <.input
-        type="select"
-        id="filter_on"
-        name="filter_on"
-        options={@options}
-        value={@selected}
-        class="px-2 py-0.5 !w-fit !inline-block pr-8 text-sm"
-      />
-    </form>
-    """
+    render_changer(assigns, "cases-filter", "change-filter", "filter_on")
   end
 
   def team_changer(assigns) do
     assigns = assign(assigns, :options, team_options())
-
-    ~H"""
-    <form data-role="team-filter" class="hidden sm:inline" phx-change="change-team">
-      <.input
-        type="select"
-        id="team_filter"
-        name="team_filter"
-        options={@options}
-        value={@selected}
-        class="px-2 py-0.5 !w-fit !inline-block pr-8 text-sm"
-      />
-    </form>
-    """
+    render_changer(assigns, "team-filter", "change-team", "team_filter")
   end
 
   def sort_changer(assigns) do
     assigns = assign(assigns, :options, sort_options())
+    render_changer(assigns, "cases-sort", "change-sort", "sort_by")
+  end
+
+  defp render_changer(assigns, data_role, event, input_id) do
+    assigns = assign(assigns, data_role: data_role, event: event, input_id: input_id)
 
     ~H"""
-    <form data-role="cases-sort" class="hidden sm:inline" phx-change="change-sort">
+    <form data-role={@data_role} class="hidden sm:inline" phx-change={@event}>
       <.input
         type="select"
-        id="sort_by"
-        name="sort_by"
+        id={@input_id}
+        name={@input_id}
         options={@options}
         value={@selected}
         class="px-2 py-0.5 !w-fit !inline-block pr-8 text-sm"
@@ -218,10 +171,6 @@ defmodule CaseManagerWeb.CaseLive.Index do
     ]
   end
 
-  defp remove_empty(params) do
-    Enum.filter(params, fn {_key, val} -> val != "" end)
-  end
-
   defp subscribe_to_topics(user) do
     if user.team_type == :mssp do
       Endpoint.subscribe("case:created")
@@ -231,5 +180,18 @@ defmodule CaseManagerWeb.CaseLive.Index do
       Endpoint.subscribe("case:escalated:" <> user.team_id)
       Endpoint.subscribe("case:updated:" <> user.team_id)
     end
+  end
+
+  defp update_params(socket, updates) do
+    remove_empty(%{
+      q: Map.get(updates, :q, socket.assigns[:search]),
+      filter: Map.get(updates, :filter, socket.assigns[:filter]),
+      sort_by: Map.get(updates, :sort_by, socket.assigns[:sort_by]),
+      team_name: Map.get(updates, :team_name, socket.assigns[:team_filter])
+    })
+  end
+
+  defp remove_empty(params) do
+    Enum.filter(params, fn {_key, val} -> val != "" end)
   end
 end
