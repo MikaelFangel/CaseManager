41294ab6104bc88fdd130c46e8ae86b34d024043
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-03-28 12:18:38 +0100

    feat: restrict selection of role based on role

--- lib/case_manager/teams/role.ex
+++ lib/case_manager/teams/role.ex
@@ -1,4 +1,9 @@
 defmodule CaseManager.Teams.Role do
   @moduledoc false
   use Ash.Type.Enum, values: [:admin, :soc_admin, :team_admin, :soc_analyst, :team_member, :service_account]
+
+  def assignable_values(:admin), do: values()
+  def assignable_values(:soc_admin), do: Enum.reject(values(), &(&1 == :admin))
+  def assignable_values(:team_admin), do: [:team_admin, :team_member]
+  def assignable_values(_role), do: nil
 end
