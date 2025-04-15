83a22d1a9c665b0f7103c241ac6d7f5ef8dc4ca1
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-04-15 10:55:06 +0200

    chore: added missing attributes to the split layout

--- lib/case_manager_web/components/layouts.ex
+++ lib/case_manager_web/components/layouts.ex
@@ -32,6 +32,11 @@ defmodule CaseManagerWeb.Layouts do
   attr :search_placeholder, :string, default: "Search..."
   attr :left_width, :string, default: "w-1/2", doc: "Width class for left panel"
   attr :right_width, :string, default: "w-1/2", doc: "Width class for right panel"
+  attr :flash, :map
+
+  slot :top
+  slot :left
+  slot :right
 
   def split(assigns) do
     ~H"""
