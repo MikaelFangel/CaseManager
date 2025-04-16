e86e4f6babf3560c42a0cbeeaa76545c3d7e5a0b
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-04-16 15:35:54 +0200

    chore: correct sorting of cases

--- lib/case_manager/incidents.ex
+++ lib/case_manager/incidents.ex
@@ -19,7 +19,7 @@ defmodule CaseManager.Incidents do
 
     resource CaseManager.Incidents.Case do
       define :create_case, action: :create
-      define :list_case, action: :read, default_options: [load: :company]
+      define :list_case, action: :read, default_options: [load: :company, query: [sort: [updated_at: :desc]]]
       define :get_case, action: :read, get_by: :id
       define :update_case, action: :update
       define :delete_case, action: :delete
