1d9905dbda1514ab9b9106777d83e6740a2432c4
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-05-09 15:13:14 +0200

    chore: correct redirect

--- lib/case_manager_web/live_user_auth.ex
+++ lib/case_manager_web/live_user_auth.ex
@@ -32,7 +32,7 @@ defmodule CaseManagerWeb.LiveUserAuth do
 
   def on_mount(:live_no_user, _params, _session, socket) do
     if socket.assigns[:current_user] do
-      {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/")}
+      {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-out")}
     else
       {:cont, assign(socket, :current_user, nil)}
     end
